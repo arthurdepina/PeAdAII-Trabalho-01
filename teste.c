1a5f892a6c4cffb47625b3163ad7921062ef083d
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2023-09-16 23:52:15 -0300

    more of that good ol' getting ready

--- teste.c
+++ teste.c
@@ -1,42 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void backup(int *pendrive_a, int *pendrive_b, int space, int *arquivos, int n_arquivos)
+int backup(int *thubmdrive_a, int *thumbdrive_b, int size, int *files, int n_files, int count)
 {
-    
+    /*
+     * thumbdrive_a and thumbdrive_b are arrays in which the files will be stored.
+     * size is the max size that each thumbdrive can store.
+     * n_files is the lenght of the thumbdrive arrays and also the number of files that
+     * have to be stored in the thumbdrives.
+    */
+    return 0;
 }
 
 int main()
 {
     int q[] = {64, 4, 128, 6};
+    /*
+        This array provides info about the thumb drives and
+        about the number of files for each test.
+        q[0] and q[2] are the sizes for thumbdrive_a and thumbdrive_b combined
+    */
 
     int len_q    = sizeof(q)/sizeof(q[0]);
-    int n_testes = len_q / 2;
+    int n_tests = len_q / 2;
 
-    int a[] = {13, 7, 30, 10,           // tamanho dos arquivos
-               44, 36, 18, 8, 9, 23};   // tamanho dos arquivos
-    
-    int len = q[1];                         // dois pendrives, A e B
-    int *pendrive_a = (int*) malloc(len * sizeof(int));
-    int *pendrive_b = (int*) malloc(len * sizeof(int));
-    int size_p = q[0] / 2;
+    int a[] = {13, 7, 30, 10,           // file sizes
+               44, 36, 18, 8, 9, 23};   // file sizes
 
-    backup(pendrive_a, pendrive_b, size_p, a, len);
-    //                               32        4
-
-    free(pendrive_a);
-    free(pendrive_b);
+    for (int test = 0; test <= n_tests; test = test + 2)
+    {
+        int size_thumbdrive = q[test] / 2;
+        int len_thumbdrive_array =  q[test + 1];
+        int *thumbdrive_a = (int*) malloc(len_thumbdrive_array * sizeof(int));
+        int *thumbdrive_b = (int*) malloc(len_thumbdrive_array * sizeof(int));
+        // backup(thumbdrive_a, thumbdrive_b, size_thumbdrive, a, len_thumbdrive_array, 0);
+        free(thumbdrive_a);
+        free(thumbdrive_b);
+    }
     return 0;
 }
 
-
-
-
-
-
-
-
 int store_a()
+    /*
+        printf("TESTE %d\n", test);
+        printf("CAPACIDADE (GB) DOS PENDRIVES: %d\n", size_thumbdrive);
+        printf("CAPACIDADE (EM PASTAS) DOS PENDRIVES: %d\n", len_thumbdrive_array);
+    */
 {
     int a[] = {
                 2,          // quantidade de testes
