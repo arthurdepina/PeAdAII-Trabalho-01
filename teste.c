1d209c1e8f6094a0fca338d2e27b76e3e162a6c2
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2023-09-17 03:12:03 -0300

    Update teste.c
    working! for the first test, that is.

--- teste.c
+++ teste.c
@@ -2,25 +2,61 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-bool worked(int *thumbdrive_a, int *thumbdrive_b, int s_a, int s_b, int n_a, int n_b) //, int size_files)
-{
-    /*
-     * Verifica se o tamanho das pastas em A não excedeu a capacidade (em GB) de A.
-     * Verifica se o tamanho das pastas em B não excedeu a capacidade (em GB) de B.
+bool found;
+
+void exibir_vetor(int *v, int len){
+	for (int i = 0; i < len; i++){
+		if (i == 0) printf("[");
+		if (i == len - 1) printf("%d]", v[i]);
+		else printf("%d, ", v[i]);
+	}
+}
+
+bool worked(int *thumbdrive_a, int *thumbdrive_b, int size_GB, int n_files_a, int n_files_b) //, int size_files)
+{   /*
+     * Verify if the size of the files in A did not exceed the capacity of A (in GB)
+     * Verify if the size of the files in B did not exceed the capacity of B (in GB)
      *      tamanho_atual_A = thumbdrive_a[0] + thumbdrive_a[1] + ... + thumbdrive_a[n_a] < s_a
      *      tamanho_atual_B = thumbdrive_b[0] + thumbdrive_b[1] + ... + thumbdrive_b[n_b] < s_b
     */
+    int current_size_a = 0, current_size_b = 0;
+
+    for (int file = 0; file < n_files_a; file++) {
+        current_size_a = current_size_a + thumbdrive_a[file];
+    }
+    for (int file = 0; file < n_files_b; file++) {
+        current_size_b = current_size_b + thumbdrive_b[file];
+    }
+
+    if (current_size_a <= size_GB && current_size_b <= size_GB) return true;
+    else return false;
 }
 
-int backup(int *thubmdrive_a, int *thumbdrive_b, int size, int *files, int n_files, int count)
-{
-    /*
+void backup(int *thumbdrive_a, int *thumbdrive_b, int sizeGB, int *files, int n_files, int count_a, int count_b, int count_files)
+{   /*
      * thumbdrive_a and thumbdrive_b are arrays in which the files will be stored.
-     * size is the max size that each thumbdrive can store.
+     * sizeGB is the max size that each thumbdrive can store in GB.
      * n_files is the lenght of the thumbdrive arrays and also the number of files that
      * have to be stored in the thumbdrives.
     */
-    return 0;
+    if (found) return;
+
+    if (count_files == n_files) {
+        if (worked(thumbdrive_a, thumbdrive_b, sizeGB, count_a, count_b)){
+            printf("thumbdrive_a: "); exibir_vetor(thumbdrive_a, count_a); printf("\n");
+            printf("thumbdrive_b: "); exibir_vetor(thumbdrive_b, count_b); printf("\n");
+            found = true;
+            printf("\n"); return;
+        } else {
+            return;
+        }
+    }
+
+    thumbdrive_a[count_a] = files[count_files];
+    backup(thumbdrive_a, thumbdrive_b, sizeGB, files, n_files, count_a + 1, count_b, count_files + 1);
+
+    thumbdrive_b[count_b] = files[count_files];
+    backup(thumbdrive_a, thumbdrive_b, sizeGB, files, n_files, count_a, count_b + 1, count_files + 1);
 }
 
 int main()
@@ -49,9 +85,10 @@ int main()
         // int size_files = get_size(a, len_thumbdrive_array)
         int *thumbdrive_a = (int*) malloc(len_thumbdrive_array * sizeof(int));
         int *thumbdrive_b = (int*) malloc(len_thumbdrive_array * sizeof(int));
-        // backup(thumbdrive_a, thumbdrive_b, size_thumbdrive, a, len_thumbdrive_array, 0);
+        backup(thumbdrive_a, thumbdrive_b, size_thumbdrive, a, len_thumbdrive_array, 0, 0, 0);
         free(thumbdrive_a);
         free(thumbdrive_b);
+        break;
     }
     return 0;
 }
